fcb5e3d2fc6019d8620e39630809b35c61337fef
Danis Zamaliev <daniszamaliev@gmail.com>
2020-12-17 22:22:04 +0300

    add 15 fix tree1

--- src/Part2.hs
+++ src/Part2.hs
@@ -11,13 +11,14 @@ prob6 :: ColorLetter -> Char
 prob6 RED = 'R'
 prob6 GREEN = 'G'
 prob6 BLUE = 'B'
+
 ------------------------------------------------------------
 -- PROBLEM #7
 --
 -- Написать функцию, которая проверяет, что значения
 -- находятся в диапазоне от 0 до 255 (границы входят)
 prob7 :: ColorPart -> Bool
-prob7 color = case color of 
+prob7 color = case color of
   Red x -> x >= 0 && x < 255
   Green x -> x >= 0 && x < 255
   Blue x -> x >= 0 && x < 255
@@ -28,10 +29,10 @@ prob7 color = case color of
 -- Написать функцию, которая добавляет в соответствующее
 -- поле значения Color значение из ColorPart
 prob8 :: Color -> ColorPart -> Color
-prob8 color part= case part of 
-  Red x -> color {red = red color + x} 
-  Green x -> color {green = green color + x} 
-  Blue x -> color {blue = blue color + x} 
+prob8 color part = case part of
+  Red x -> color {red = red color + x}
+  Green x -> color {green = green color + x}
+  Blue x -> color {blue = blue color + x}
 
 ------------------------------------------------------------
 -- PROBLEM #9
@@ -39,10 +40,10 @@ prob8 color part= case part of
 -- Написать функцию, которая возвращает значение из
 -- ColorPart
 prob9 :: ColorPart -> Int
-prob9 color  =  case color of 
-    Red x -> x
-    Green x -> x
-    Blue x -> x
+prob9 color = case color of
+  Red x -> x
+  Green x -> x
+  Blue x -> x
 
 ------------------------------------------------------------
 -- PROBLEM #10
@@ -50,17 +51,17 @@ prob9 color  =  case color of
 -- Написать функцию, которая возвращает компонент Color, у
 -- которого наибольшее значение (если такой единственный)
 prob10 :: Color -> Maybe ColorPart
-prob10 color 
-    | red color > green color && red color > blue color = Just (Red (red color))
-    | green color > blue color && green color > red color = Just (Green (green color))
-    | blue color > green color && blue color > red color = Just (Blue (blue color))
-    | otherwise = Nothing
+prob10 color
+  | red color > green color && red color > blue color = Just (Red (red color))
+  | green color > blue color && green color > red color = Just (Green (green color))
+  | blue color > green color && blue color > red color = Just (Blue (blue color))
+  | otherwise = Nothing
 
 ------------------------------------------------------------
 -- PROBLEM #11
 --
 -- Найти сумму элементов дерева
---TODO неправильный тест ??? Tree 
+--TODO неправильный тест ??? Tree
 -- {left = Just (Tree {left = Nothing, root = 1, right = Nothing}),
 -- root = 3, right = Just (Tree {left = Just (Tree {left = Nothing, root = 5, right = Nothing}),
 -- root = 6, right = Nothing})}
@@ -68,12 +69,11 @@ prob10 color
 prob11 :: Num a => Tree a -> a
 prob11 tree = sum (toList tree)
 
-
 toList :: Tree a -> [a]
 toList tree = [root tree] ++ maybeToList (left tree) ++ maybeToList (right tree)
-   where maybeToList (Just x) = toList x
-         maybeToList Nothing  = []
-        
+  where
+    maybeToList (Just x) = toList x
+    maybeToList Nothing = []
 
 ------------------------------------------------------------
 -- PROBLEM #12
@@ -86,14 +86,14 @@ toList tree = [root tree] ++ maybeToList (left tree) ++ maybeToList (right tree)
 prob12 :: Ord a => Tree a -> Bool
 prob12 tree = checkRight (right tree) (root tree) && checkLeft (left tree) (root tree)
 
-
 checkRight :: Ord a => Maybe (Tree a) -> a -> Bool
 checkRight Nothing x = True
-checkRight (Just tree) parent = root tree >= parent  && checkLeft (left tree) (root tree) && checkRight (right tree) (root tree)
+checkRight (Just tree) parent = root tree >= parent && checkLeft (left tree) (root tree) && checkRight (right tree) (root tree)
 
 checkLeft :: Ord a => Maybe (Tree a) -> a -> Bool
 checkLeft Nothing x = True
 checkLeft (Just tree) parent = root tree < parent && checkLeft (left tree) (root tree) && checkRight (right tree) (root tree)
+
 ------------------------------------------------------------
 -- PROBLEM #13
 --
@@ -119,18 +119,46 @@ prob14 :: Tree () -> Tree Int
 prob14 tree = changeToInt tree 1
 
 changeToInt :: Tree () -> Int -> Tree Int
-changeToInt tree currentNum = Tree {left = getTree (left tree) (currentNum+2), root=currentNum, right=getTree (right tree) (currentNum+1)}
+changeToInt tree currentNum = Tree {left = leftTree, root = num, right = rightTree}
+  where
+    rightTree = getTree (right tree) currentNum
+    num = getRootNum rightTree
+      where
+        getRootNum :: Maybe (Tree Int) -> Int
+        getRootNum Nothing = currentNum
+        getRootNum (Just tree) = root tree + 1
+    leftTree = getTree (left tree) (num + 1)
 
 getTree :: Maybe (Tree ()) -> Int -> Maybe (Tree Int)
 getTree Nothing x = Nothing
-getTree (Just tree) currentNum = Just (Tree {left = getTree (left tree) (currentNum+2), root=currentNum, right=getTree (right tree) (currentNum+1)})
+getTree (Just tree) currentNum = Just (Tree {left = leftTree, root = num, right = rightTree})
+  where
+    rightTree = getTree (right tree) currentNum
+    num = getRootNum rightTree
+      where
+        getRootNum :: Maybe (Tree Int) -> Int
+        getRootNum Nothing = currentNum
+        getRootNum (Just tree) = root tree + 1
+    leftTree = getTree (left tree) (num + 1)
+
 ------------------------------------------------------------
 -- PROBLEM #15
 --
 -- Выполнить вращение дерева влево относительно корня
 -- (https://en.wikipedia.org/wiki/Tree_rotation)
 prob15 :: Tree a -> Tree a
-prob15 = error "Implement me!"
+prob15 tree = Tree {left = leftTree, root = num, right = rightTree}
+  where
+    q = getRight (right tree)
+      where
+        getRight :: Maybe (Tree a) -> Tree a
+        getRight Nothing = error "No right tree"
+        getRight (Just treeQ) = treeQ
+
+    rightTreeB = left q
+    leftTree = Just (Tree {left = left tree, root = root tree, right = rightTreeB})
+    num = root q
+    rightTree = right q
 
 ------------------------------------------------------------
 -- PROBLEM #16
